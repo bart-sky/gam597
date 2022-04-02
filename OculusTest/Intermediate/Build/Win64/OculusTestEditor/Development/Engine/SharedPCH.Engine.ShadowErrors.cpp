59a0de0d877984930809dbba8b66607505597491
mwager2 <mwager2@depaul.edu>
2022-04-02 16:19:08 -0400

    -Base Oculus Test project

---
+++ OculusTest/Intermediate/Build/Win64/OculusTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/Mark_School/Documents/Masters/GAM597_AdvancedVRGameDev/gam597/OculusTest/Intermediate/Build/Win64/OculusTestEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ OculusTest/Intermediate/Build/Win64/UE4Editor/Inc/OculusTest/OculusTestGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OCULUSTEST_OculusTestGameModeBase_generated_h
+#error "OculusTestGameModeBase.generated.h already included, missing '#pragma once' in OculusTestGameModeBase.h"
+#endif
+#define OCULUSTEST_OculusTestGameModeBase_generated_h
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_SPARSE_DATA
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_RPC_WRAPPERS
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAOculusTestGameModeBase(); \
+	friend struct Z_Construct_UClass_AOculusTestGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AOculusTestGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OculusTest"), NO_API) \
+	DECLARE_SERIALIZER(AOculusTestGameModeBase)
+
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAOculusTestGameModeBase(); \
+	friend struct Z_Construct_UClass_AOculusTestGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AOculusTestGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/OculusTest"), NO_API) \
+	DECLARE_SERIALIZER(AOculusTestGameModeBase)
+
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOculusTestGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOculusTestGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOculusTestGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOculusTestGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOculusTestGameModeBase(AOculusTestGameModeBase&&); \
+	NO_API AOculusTestGameModeBase(const AOculusTestGameModeBase&); \
+public:
+
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOculusTestGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOculusTestGameModeBase(AOculusTestGameModeBase&&); \
+	NO_API AOculusTestGameModeBase(const AOculusTestGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOculusTestGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOculusTestGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOculusTestGameModeBase)
+
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_12_PROLOG
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_SPARSE_DATA \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_RPC_WRAPPERS \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_INCLASS \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_SPARSE_DATA \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	OculusTest_Source_OculusTest_OculusTestGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> OCULUSTEST_API UClass* StaticClass<class AOculusTestGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID OculusTest_Source_OculusTest_OculusTestGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
